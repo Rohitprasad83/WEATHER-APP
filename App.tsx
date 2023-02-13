a789a188d5574319c07405affcf8ee99a6649c3d
Rohit Prasad <pdrohit674@gmail.com>
2023-02-13 23:57:34 +0530

    feat: added geolocation and linking

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking,Button } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
 export default function App() {
+  const openSettings = () => {
+    Linking.openSettings();
+  };
+  
+  const askForLocationPermission = () => {
+    Geolocation.requestAuthorization();
+    Geolocation.getCurrentPosition(
+      position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      },
+      error => {
+        console.log(error.message);
+      },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Enable Location Services" onPress={() => {
+          askForLocationPermission();
+          openSettings();
+      }} />
       <StatusBar style="auto" />
     </View>
   );
